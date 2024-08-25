cacbc5fcaf217c77dc99feb6adb5f93c1f64646d
dariabadea4 <164878313+dariabadea4@users.noreply.github.com>
2024-08-25 20:29:02 +0000

    Deploying to gh-pages from @ dariabadea4/goit-js-hw-09@95720ded2547f571bf354006f0356138ba6f8f09 🚀

---
+++ 01-color-switcher.e2bc4665.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");let a=null;const l=()=>`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.addEventListener("click",(()=>{e.style.backgroundColor=l(),a=setInterval((()=>{e.style.backgroundColor=l()}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.e2bc4665.js.map
